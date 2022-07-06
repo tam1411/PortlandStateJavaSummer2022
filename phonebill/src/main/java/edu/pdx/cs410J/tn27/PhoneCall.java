b555c4beeb612487d895c536dfb798d6110dce50
tam1411 <tn27@pdx.edu>
2022-07-06 13:39:08 -0700

    Added basic unit tests to the PhoneCallTest.

--- phonebill/src/main/java/edu/pdx/cs410J/tn27/PhoneCall.java
+++ phonebill/src/main/java/edu/pdx/cs410J/tn27/PhoneCall.java
@@ -3,23 +3,33 @@ package edu.pdx.cs410J.tn27;
 import edu.pdx.cs410J.AbstractPhoneCall;
 
 public class PhoneCall extends AbstractPhoneCall {
+  private final String caller_number;
+  private final String callee_number;
+  private final String begin_time;
+  private String end_time;
+  public PhoneCall(String caller_number, String callee_number, String begin_time, String end_time){
+    this.caller_number = caller_number;
+    this.callee_number = callee_number;
+    this.begin_time = begin_time;
+    this.end_time = end_time;
+  }
   @Override
   public String getCaller() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
+    return this.caller_number;
   }
 
   @Override
   public String getCallee() {
-    return "This method is not implemented yet";
+    return this.callee_number;
   }
 
   @Override
   public String getBeginTimeString() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
+    return this.begin_time;
   }
 
   @Override
   public String getEndTimeString() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
+    return this.end_time;
   }
 }
