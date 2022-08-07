83c3f279774756174638819416b0ba6f88a3692b
tam1411 <tn27@pdx.edu>
2022-08-06 23:04:24 -0700

    add a button

--- Phonebill2/app/src/main/java/edu/pdx/cs410J/tn27/MainActivity.java
+++ Phonebill2/app/src/main/java/edu/pdx/cs410J/tn27/MainActivity.java
@@ -3,6 +3,8 @@ package edu.pdx.cs410J.tn27;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sayHello(View view) {
+        Toast.makeText(this,"Hello!", Toast.LENGTH_SHORT).show();
     }
 }
