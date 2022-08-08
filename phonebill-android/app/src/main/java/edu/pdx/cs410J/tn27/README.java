0e3cf057a706dee1073527ebcf1bab4413c18cb4
tam1411 <tn27@pdx.edu>
2022-08-08 14:52:02 -0700

    Working on Readme

---
+++ phonebill-android/app/src/main/java/edu/pdx/cs410J/tn27/README.java
@@ -0,0 +1,52 @@
+package edu.pdx.cs410J.tn27;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class README extends AppCompatActivity {
+    TextView txt;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_readme);
+        txt = findViewById(R.id.Readme);
+        txt.setText(readme().toString());
+        //Toast.makeText(this, )
+    }
+
+
+    private StringBuilder readme(){
+        StringBuilder txt = new StringBuilder();
+        try {
+            InputStream readme = getResources().openRawResource(R.raw.readme);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(readme));
+            String line = null;
+
+            while ((line = reader.readLine()) != null) {
+                txt.append(line);
+                txt.append("\n");
+            }
+
+        } catch (IOException e) {
+            ReadmeException(e);
+        }
+        return txt;
+    }
+
+    private void ReadmeException(IOException e) {
+
+    }
+
+}
---
+++ phonebill-android/app/src/main/res/raw/readme.txt
@@ -0,0 +1,27 @@
+                                      Welcome to Phone Bill Help.
+Phone Bill app contains:
+1. Create a phone bill
+2. Add a phone call
+3. Search for calls in a phone bill in a time range
+4. Pretty print a phone bill with phone calls.
+
+Please follow the format dd/mm/yyyy and n:nn am/pm for date and time
+To Create a phone bill:
+1. Enter the customer name.
+2. Enter all the caller, callee, date begin, time begin, end date, end time.
+
+
+To Add a phone call.
+1. Enter the customer name to search for existing phone bill
+2. Add a phone call following the above format.
+System will automatically create a new phone bill if non-exist.
+
+To Search for a call in a phone bill
+1. Enter the customer name
+2. Enter begin date/time
+3. Enter end date/time
+Non exist customer name will display message as "No Found".
+
+To pretty print content of a phone bill
+1. Enter the customer name.
+Non exist customer name will display message as "No Found".
