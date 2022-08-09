aaaad9829b4d13aa781fa2e7a794e37284426873
tam1411 <tn27@pdx.edu>
2022-08-08 20:29:08 -0700

    Added features to Create Phone Bill

---
+++ phonebill-android/app/src/main/java/edu/pdx/cs410J/tn27/LaunchCreateNewPhoneBill.java
@@ -0,0 +1,14 @@
+package edu.pdx.cs410J.tn27;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LaunchCreateNewPhoneBill extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch_create_new_phone_bill);
+    }
+}
--- phonebill-android/app/src/main/java/edu/pdx/cs410J/tn27/LaunchMenu.java
+++ phonebill-android/app/src/main/java/edu/pdx/cs410J/tn27/LaunchMenu.java
@@ -2,9 +2,13 @@ package edu.pdx.cs410J.tn27;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+/**
+ * Class to launch the menu interface
+ */
 public class LaunchMenu extends AppCompatActivity {
 
     @Override
@@ -14,7 +18,8 @@ public class LaunchMenu extends AppCompatActivity {
     }
 
     public void LaunchCreatePhoneBill(View view){
-
+        Intent intent = new Intent(this, LaunchCreateNewPhoneBill.class);
+        startActivity(intent);
     }
 
 
