87f9e9078f512806df0710872d79c86b194a00f0
tam1411 <tn27@pdx.edu>
2022-08-09 18:36:56 -0700

    Launch The Add_New_Call

---
+++ phonebill-android/app/src/main/java/edu/pdx/cs410J/tn27/Launch_Add_New_call.java
@@ -0,0 +1,14 @@
+package edu.pdx.cs410J.tn27;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Launch_Add_New_call extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch_add_new_call);
+    }
+}
