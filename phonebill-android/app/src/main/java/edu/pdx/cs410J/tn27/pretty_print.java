fabe695c32faa7819c627fae91c156f434f2328f
tam1411 <tn27@pdx.edu>
2022-08-10 16:19:50 -0700

    Have pretty print worked

--- phonebill-android/app/src/main/java/edu/pdx/cs410J/tn27/pretty_print.java
+++ phonebill-android/app/src/main/java/edu/pdx/cs410J/tn27/pretty_print.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 import edu.pdx.cs410J.ParserException;
 
@@ -24,8 +27,8 @@ public class pretty_print extends AppCompatActivity {
         setContentView(R.layout.activity_pretty_print);
     }
 
-    public boolean SearchPhoneCallPrettyPrint(View view) {
-        EditText Customer = findViewById(R.id.editTextTextPersonName);
+    public void SearchPhoneCallPrettyPrint(View view) {
+        EditText Customer = findViewById(R.id.CustomerPretty);
         if (Customer.length() == 0) {
             Customer.setError("This field is required");
         } else {
@@ -35,31 +38,34 @@ public class pretty_print extends AppCompatActivity {
             if (!IsExist) {
                 Toast.makeText(this, "No Found Customer", Toast.LENGTH_LONG).show();
             } else {
-                Toast.makeText(this, "Customer :" + customer, Toast.LENGTH_LONG).show();
-                return true;
+                Toast.makeText(this, "Customer :" + customer, Toast.LENGTH_SHORT).show();
+                //return true;
+                try {
+                    TextParser parser = new TextParser(new FileReader(file));
+                    PhoneBill bill = parser.parse();
+                    Print(bill);
+                    Toast.makeText(this,"Load successfully", Toast.LENGTH_LONG).show();
+
+                }catch(ParserException | FileNotFoundException e){
+                    e.getMessage();
+                }
 
             }
         }
-        return false;
 
     }
 
-   public void PrettyPrint(View view) {
-       if (SearchPhoneCallPrettyPrint(view)){
-           Intent intent = new Intent(this,ToPrint.class);
-           startActivity(intent);
+   public void Print(PhoneBill bill) {
+       if (bill != null) {
+            ArrayList<PhoneCall> call_list = (ArrayList<PhoneCall>) bill.getPhoneCalls();
 
-
-            /*EditText Customer = findViewById(R.id.editTextTextPersonName);
-            String customer = Customer.getText().toString();
-            File file_name = getFile(customer);
-          try {
-              TextParser parser = new TextParser(new FileReader(file_name));
-              PhoneBill bill = parser.parse();
-          }catch(ParserException | FileNotFoundException e){
-              e.getMessage();
-          }*/
-        }
+            ListView listview = findViewById(R.id.view);
+            ArrayAdapter<String> all_calls = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+            for (int i = 0; i< call_list.size(); ++i){
+                all_calls.add(call_list.get(i).toString());
+            }
+            listview.setAdapter(all_calls);
+       }
     }
         private File getFile(String Customer){
             String file_name =String.format("%s.txt",Customer);
