7ca1bf57737bbd6cb35345145a040120701a9e3c
tam1411 <tn27@pdx.edu>
2022-08-10 18:49:39 -0700

    Add Search between date/time features

---
+++ phonebill-android/app/src/main/java/edu/pdx/cs410J/tn27/SearchBetweenDate.java
@@ -0,0 +1,14 @@
+package edu.pdx.cs410J.tn27;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchBetweenDate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_between_date);
+    }
+}
--- phonebill-web/src/main/java/edu/pdx/cs410J/tn27/Messages.java
+++ phonebill-web/src/main/java/edu/pdx/cs410J/tn27/Messages.java
@@ -1,5 +1,8 @@
 package edu.pdx.cs410J.tn27;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 /**
  * Class for formatting messages on the server side.  This is mainly to enable
  * test methods that validate that the server returned expected strings.
@@ -14,6 +17,7 @@ public class Messages
     public static String CustomerhasPhoneBill(String customer, PhoneCall new_call )
     {
         return String.format( "%s\n%s\n", customer, new_call.toString() );
+        Collection<Object>strings = new ArrayList<String>();
     }
 
     public static String allDictionaryEntriesDeleted() {
